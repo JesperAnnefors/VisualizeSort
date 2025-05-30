a26e06f39c974bc26a180c85f525f1461620176d
Jesper Annefors <jesper.annefors@hotmail.com>
2025-05-30 12:58:01 +0200

    Some cleanup.

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -13,59 +13,59 @@ import javax.swing.JSlider;
 
 public class MainFrame {
 
-		public MainFrame() {
-			//create a frame
-			JFrame frame = new JFrame();
-			
-			//set frame specifications
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setTitle("VisualizeSort");
-			frame.setResizable(false);
-			frame.setSize(1200, 720);
-			frame.setLayout(new BorderLayout());
-			frame.setLocationRelativeTo(null);
-			
-			//create a header panel
-			JPanel header = new JPanel();
-			
-			//set header specifications
-			header.setPreferredSize(new Dimension(1200, 81));
-			header.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 15));
-			
-			//create header components
-			JButton randomize = new JButton("Randomize");
-			
-			JButton sort = new JButton("Sort!");
-			
-			String[] algorithms = {"Bubble sort", "Merge sort"};
-			JComboBox<String> algorithm = new JComboBox<String>(algorithms);
-			
-			JSlider slider = new JSlider(10, 100, 50);
-			slider.setPaintTicks(true);
-			slider.setMinorTickSpacing(5);
-			slider.setPaintTrack(true);
-			slider.setMajorTickSpacing(10);
-			slider.setPaintLabels(true);
-			
-			JLabel label = new JLabel(" Array Length: " + slider.getValue() + " ");
-			label.setBorder(BorderFactory.createLineBorder(Color.black));
-			
-			//add components to header
-			header.add(randomize);
-			header.add(algorithm);
-			header.add(sort);
-			header.add(slider);
-			header.add(label);
-			
-			//Create AlgoSortPanel
-			AlgoSortPanel sortPanel = new AlgoSortPanel();
-			
-			sortPanel.setBackground(Color.blue);
-			
-			//add panels to frame
-			frame.add(header, BorderLayout.NORTH);
-			frame.add(sortPanel, BorderLayout.CENTER);
-			
-			frame.setVisible(true);
-		}
+	public MainFrame() {
+		//create a frame
+		JFrame frame = new JFrame();
+		
+		//set frame specifications
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("VisualizeSort");
+		frame.setResizable(false);
+		frame.setSize(1200, 720);
+		frame.setLayout(new BorderLayout());
+		frame.setLocationRelativeTo(null);
+		
+		//create a header panel
+		JPanel header = new JPanel();
+		
+		//set header specifications
+		header.setPreferredSize(new Dimension(1200, 81));
+		header.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 15));
+		
+		//create header components
+		JButton randomize = new JButton("Randomize");
+		
+		JButton sort = new JButton("Sort!");
+		
+		String[] algorithms = {"Bubble sort", "Merge sort"};
+		JComboBox<String> algorithm = new JComboBox<String>(algorithms);
+		
+		JSlider slider = new JSlider(10, 100, 50);
+		slider.setPaintTicks(true);
+		slider.setMinorTickSpacing(5);
+		slider.setPaintTrack(true);
+		slider.setMajorTickSpacing(10);
+		slider.setPaintLabels(true);
+		
+		JLabel label = new JLabel(" Array Length: " + slider.getValue() + " ");
+		label.setBorder(BorderFactory.createLineBorder(Color.black));
+		
+		//add components to header
+		header.add(randomize);
+		header.add(algorithm);
+		header.add(sort);
+		header.add(slider);
+		header.add(label);
+		
+		//Create AlgoSortPanel
+		AlgoSortPanel sortPanel = new AlgoSortPanel();
+		
+		sortPanel.setBackground(Color.blue);
+		
+		//add panels to frame
+		frame.add(header, BorderLayout.NORTH);
+		frame.add(sortPanel, BorderLayout.CENTER);
+		
+		frame.setVisible(true);
+	}
 }
